08502c342186f3ab2c8f25393e72d33a4b080983
Roman Kruchinin <kreativ87e@gmail.com>
2022-10-13 17:41:57 +0600

    Сделано 4 задание и решены проблемы со словарями

--- university.py
+++ university.py
@@ -9,18 +9,34 @@ class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
         self.grades = {}
+        self.average_grade = 0
 
-    def __str__(self):
+    def find_average_grade(self):
         s = 0
         n = 0
         for course in self.grades:
             s += sum(self.grades[course])
             n += len(self.grades[course])
-        if n == 0:
-            return 'Имя: {}\nФамилия: {}\nСредняя оценка за лекции: {}'.format(self.name, self.surname, 0)
-        else:
-            return 'Имя: {}\nФамилия: {}\nСредняя оценка за лекции: {}'.format(self.name, self.surname, s/n)
+        self.average_grade = s/n
+  
+    def __str__(self):
+        return 'Имя: {}\nФамилия: {}\nСредняя оценка за лекции: {}'.format(self.name, self.surname, self.average_grade)
 
+    def __eq__(self, other):
+        if self.average_grade == other.average_grade:
+            return True
+        else:
+            return False
+    def __lt__(self, other):
+        if self.average_grade < other.average_grade:
+            return True
+        else:
+            return False
+    def __gt__(self, other):
+        if self.average_grade > other.average_grade:
+            return True
+        else:
+            return False
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
@@ -47,18 +63,18 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
-        
+        self.average_grade = 0
 
-    def __str__(self):
+    def find_average_grade(self):
         s = 0
         n = 0
         for course in self.grades:
             s += sum(self.grades[course])
             n += len(self.grades[course])
-        if n == 0:
-            return 'Имя: {}\nФамилия: {}\nСредняя оценка за домашние задания: {}\nКурсы в процессе изучения: {}\nЗавершенные курсы: {}'.format(self.name, self.surname, 0, ', '.join(self.courses_in_progress),', '.join(self.finished_courses))
-        else:
-            return 'Имя: {}\nФамилия: {}\nСредняя оценка за домашние задания: {}\nКурсы в процессе изучения: {}\nЗавершенные курсы: {}'.format(self.name, self.surname, s/n, ', '.join(self.courses_in_progress),', '.join(self.finished_courses))
+        self.average_grade = s/n
+    
+    def __str__(self):
+        return 'Имя: {}\nФамилия: {}\nСредняя оценка за домашние задания: {}\nКурсы в процессе изучения: {}\nЗавершенные курсы: {}'.format(self.name, self.surname, self.average_grade, ', '.join(self.courses_in_progress),', '.join(self.finished_courses))
 
     def rate_lec(self, lecturer, course, grade):
         if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached:
@@ -68,13 +84,31 @@ class Student:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+          
+    def __eq__(self, other):
+        if self.average_grade == other.average_grade:
+            return True
+        else:
+            return False
+    def __lt__(self, other):
+        if self.average_grade < other.average_grade:
+            return True
+        else:
+            return False
+    def __gt__(self, other):
+        if self.average_grade > other.average_grade:
+            return True
+        else:
+            return False
+      
 
 def average_stud(students, course):
     s = 0
     n = 0
     for stud in students:
-        s += sum(stud.grades[course])
-        n += len(stud.grades[course])
+        if course in stud.grades:
+            s += sum(stud.grades[course])
+            n += len(stud.grades[course])
     if n == 0:
         return 0
     else:
@@ -84,8 +118,9 @@ def average_lect(lectors, course):
     s = 0
     n = 0
     for lect in lectors:
-        s += sum(lect.grades[course])
-        n += len(lect.grades[course])
+        if course in lect.grades:
+            s += sum(lect.grades[course])
+            n += len(lect.grades[course])
     if n == 0:
         return 0
     else:
@@ -118,22 +153,50 @@ review1.rate_hw(stud1, 'Java', 10)
 review1.rate_hw(stud1, 'Python', 5)
 review1.rate_hw(stud2, 'Java', 7)
 review1.rate_hw(stud2, 'SQL', 1)
+review1.rate_hw(stud2, 'Python', 9.5)
 
 review2 = Reviewer('Leona','Dianova')
 review2.rate_hw(stud1, 'Java', 7)
 review2.rate_hw(stud1, 'Python', 6)
 review2.rate_hw(stud2, 'Java', 9)
 review2.rate_hw(stud2, 'SQL', 10)
+review2.rate_hw(stud2, 'Python', 4.5)
+
+lect1.find_average_grade()
+lect2.find_average_grade()
 
 print("1 лектор")
 print(lect1)
+print()
 print("2 лектор")
 print(lect2)
+print()
+
+if lect1 > lect2:
+    print("{} {} лучше по средней оценке, чем {} {}".format(lect1.name,lect1.surname,lect2.name,lect2.surname))
+elif lect1 < lect2:
+    print("{} {} лучше по средней оценке, чем {} {}".format(lect2.name,lect2.surname,lect1.name,lect1.surname))
+else:
+    print("Средная оценка у лектора {} {} совпадает со средней оценкой лектора {} {}".format(lect1.name,lect1.surname,lect2.name,lect2.surname))
+print()
+
+stud1.find_average_grade()
+stud2.find_average_grade()
 
 print("1 студент")
 print(stud1)
+print()
 print("2 студент")
 print(stud2) 
+print()
+
+if stud1 > stud2:
+    print("{} {} лучше по средней оценке, чем {} {}".format(stud1.name,stud1.surname,stud2.name,stud2.surname))
+elif stud1 < stud2:
+    print("{} {} лучше по средней оценке, чем {} {}".format(stud2.name,stud2.surname,stud1.name,stud1.surname))
+else:
+    print("Средная оценка у студента {} {} совпадает со средней оценкой студента {} {}".format(stud1.name,stud1.surname,stud2.name,stud2.surname))
+print()
 
 s.append(stud1)
 s.append(stud2)
@@ -141,4 +204,8 @@ l.append(lect1)
 l.append(lect2)
 
 print("Средняя оценка за курс Python среди всех студентов -",average_stud(s,'Python'))
-
+print("Средняя оценка за лекции на курсе Python среди всех лекторов -",average_lect(s,'Python'))
+print("Средняя оценка за курс Java среди всех студентов -",average_stud(s,'Java'))
+print("Средняя оценка за лекции на курсе Java среди всех лекторов -",average_lect(s,'Java'))
+print("Средняя оценка за курс SQL среди всех студентов -",average_stud(s,'SQL'))
+print("Средняя оценка за лекции на курсе SQL среди всех лекторов -",average_lect(s,'SQL'))
